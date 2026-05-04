a9d5ca4fb4958035bfc21fb6f07b73cec930bb76
ajaySingh2615 <ajaysingh261526@gmail.com>
2026-05-04 23:57:28 +0530

    feat: add initial Node.js foundation with core modules and global type checks

---
+++ intro_to_node-01/01-node-foundation.js
@@ -0,0 +1,11 @@
+const fs = require("node:fs");
+const path = require("path");
+const os = require("os");
+
+// console.log("NodeJS: ", process.versions.node);
+// console.log("V8: ", process.versions.v8);
+// console.log("LivUV: ", process.versions.uv);
+// console.log("Plateform: ", process.platform);
+
+console.log(typeof global);
+console.log(typeof globalThis);
