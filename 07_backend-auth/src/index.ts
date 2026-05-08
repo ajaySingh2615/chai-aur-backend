85098c395ddd16acc814f685812d0d2fb8076722
ajaySingh2615 <ajaysingh261526@gmail.com>
2026-05-08 14:55:18 +0530

    feat: set up basic Express server for authentication service
    - Implemented a new entry point in index.ts to create and start an HTTP server using Express.
    - Added a createExpressApplication function in app/index.ts to initialize the Express application with a welcome route.
    - Updated tsconfig.json to include Node types and ESNext libraries for better compatibility.

--- 07_backend-auth/src/index.ts
+++ 07_backend-auth/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello");
+import http from "node:http";
+import { createExpressApplication } from "./app/index.js";
+
+async function main() {
+  try {
+    const server = http.createServer(createExpressApplication());
+    const PORT: number = 8080;
+
+    server.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting the server:", error);
+    throw error;
+  }
+}
+
+main();
