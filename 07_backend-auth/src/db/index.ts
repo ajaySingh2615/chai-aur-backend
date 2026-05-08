f14ce828e86af44af4fe42410f0a8aa4b02f3cf3
ajaySingh2615 <ajaysingh261526@gmail.com>
2026-05-08 21:43:45 +0530

    feat: refactor database connection setup and update TypeScript configuration

--- 07_backend-auth/src/db/index.ts
+++ 07_backend-auth/src/db/index.ts
@@ -1,4 +1,8 @@
 import "dotenv/config";
+import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+export const db = drizzle(pool);
