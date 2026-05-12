0d2f05ec4e9583248373f5d0674bb43aae468085
ajaySingh2615 <ajaysingh261526@gmail.com>
2026-05-13 01:28:24 +0530

    feat: add JWT handling with access and refresh token generation

--- 07-backend-auth/backend/src/common/config/env.ts
+++ 07-backend-auth/backend/src/common/config/env.ts
@@ -4,7 +4,13 @@ import "dotenv/config";
 const envSchema = z.object({
   PORT: z.string().default("5000").transform(Number),
   NODE_ENV: z.enum(["development", "production"]).default("development"),
-  DATABASE_URL: z.string().url()
+  DATABASE_URL: z.string().url(),
+
+  // Add our new JWT rules
+  JWT_ACCESS_SECRET: z.string(),
+  JWT_REFRESH_SECRET: z.string(),
+  JWT_ACCESS_EXPIRES_IN: z.string().default("15m"),
+  JWT_REFRESH_EXPIRES_IN: z.string().default("7d"),
 });
 
 const _env = envSchema.safeParse(process.env);
---
+++ 07-backend-auth/backend/src/common/utils/jwt.ts
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import { env } from "../config/env.js";
+
+// the payload is the data we embed inside the token
+export interface JwtPayload {
+  userId: string;
+  role: string;
+}
+
+export const generateAccessToken = (payload: JwtPayload) => {
+  // 1. generate the short lived access token
+  const accessToken = jwt.sign(payload, env.JWT_ACCESS_SECRET, {
+    expiresIn: env.JWT_ACCESS_EXPIRES_IN as any,
+  });
+
+  // 2. Generate the long lived refresh token
+  const refreshToken = jwt.sign(payload, env.JWT_REFRESH_SECRET, {
+    expiresIn: env.JWT_REFRESH_EXPIRES_IN as any,
+  });
+
+  return { accessToken, refreshToken };
+};
